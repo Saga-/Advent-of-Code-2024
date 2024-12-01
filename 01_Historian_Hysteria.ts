1f3f48d6c2d511c0d0482f2962524ec1f37b8bb8
Saga- <saga@saga.com>
2024-12-01 19:09:08 +0000

    day 1 part 2 basic

--- 01_Historian_Hysteria.ts
+++ 01_Historian_Hysteria.ts
@@ -5,6 +5,7 @@ const DELIMETER = "   ";
 
 const file = readFileSync(FILE, "utf-8");
 const cols: string[] = file.split("\n");
+// PART 1
 
 // We can just declare two variabiles and call .push() inside a forEach instead of two maps()
 const leftColSorted: number[] = cols
@@ -25,4 +26,29 @@ for (let i = 0; i < leftColSorted.length; i++) {
   totalDistance += Math.abs(leftColSorted[i] - rightColSorted[i]);
 }
 
-console.log(totalDistance);
+console.log("Total Distance:", totalDistance);
+
+// PART 2
+const occurences: { [key: number]: number } = {};
+
+for (let i = 0; i < leftColSorted.length; i++) {
+  const currentVal = leftColSorted[i];
+  occurences[currentVal] = 0;
+  rightColSorted.forEach((value) => {
+    if (currentVal === value) {
+      occurences[currentVal] += 1;
+    }
+  });
+  if (occurences[currentVal] === 0) {
+    delete occurences[currentVal];
+  } else {
+    occurences[currentVal] = currentVal * occurences[currentVal];
+  }
+}
+
+const totalValue = Object.values(occurences).reduce(
+  (a, b) => (a as number) + (b as number),
+  0
+);
+
+console.log("Total Value:", totalValue);
